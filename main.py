b221d7272ddae3f811315b59a075fcd28f4d918e
Sunil RUfus <srufus09@gmail.com>
2023-04-13 02:00:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,100 @@
+# Run by typing python3 main.py
+
+# **IMPORTANT:** only collaborators on the project where you run
+# this can access this web server!
+"""
+    Bonus points if you want to have internship at AI Camp
+    1. How can we save what user built? And if we can save them, like allow them to publish, can we load the saved results back on the home page? 
+    2. Can you add a button for each generated item at the frontend to just allow that item to be added to the story that the user is building? 
+    3. What other features you'd like to develop to help AI write better with a user? 
+    4. How to speed up the model run? Quantize the model? Using a GPU to run the model? 
+"""
+
+# import basics
+import os
+
+# import stuff for our web server
+from flask import Flask, request, redirect, url_for, render_template, session
+from utils import get_base_url
+# import stuff for our models
+#from aitextgen import aitextgen
+
+# load up a model from memory. Note you may not need all of these options.
+# ai = aitextgen(model_folder="model/",
+#                tokenizer_file="model/aitextgen.tokenizer.json", to_gpu=False)
+
+#ai = aitextgen(model="distilgpt2", to_gpu=False)
+
+# setup the webserver
+# port may need to be changed if there are multiple flask servers running on same server
+port = 12345
+base_url = get_base_url(port)
+
+# if the base url is not empty, then the server is running in development, and we need to specify the static folder so that the static files are served
+if base_url == '/':
+  app = Flask(__name__)
+else:
+  app = Flask(__name__, static_url_path=base_url + 'static')
+
+app.secret_key = os.urandom(64)
+
+# set up the routes and logic for the webserver
+
+
+@app.route(f'{base_url}')
+def home():
+  return render_template('writer_home.html', generated=None)
+
+
+@app.route(f'{base_url}', methods=['POST'])
+def home_post():
+  return redirect(url_for('results'))
+
+
+@app.route(f'{base_url}/results/')
+def results():
+  if 'data' in session:
+    data = session['data']
+
+    return render_template('Write-your-story-with-AI.html', generated=data)
+  else:
+    return render_template('Write-your-story-with-AI.html', generated=None)
+
+
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/sunilrufus/NLP"
+headers = {"Authorization": "Bearer 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"}
+
+
+def query(payload):
+  response = requests.post(API_URL, headers=headers, json=payload)
+  return response.json()
+
+
+"""output = query({
+	"inputs": "Can you please let us know more details about your ",
+})"""
+
+
+@app.route(f'{base_url}/generate_text/', methods=["POST"])
+def generate_text():
+  #output = results()
+  prompt = request.form['prompt']
+  response = requests.post(API_URL, headers=headers, json=prompt)
+  session['data'] = response.json()
+  return redirect(url_for('results'))
+
+
+# define additional routes here
+# for example:
+# @app.route(f'{base_url}/team_members')
+# def team_members():
+#     return render_template('team_members.html') # would need to actually make this page
+
+if __name__ == '__main__':
+  # IMPORTANT: change url to the site where you are editing this file.
+  website_url = 'coding.ai-camp.dev'
+
+  print(f'Try to open\n\n    https://{website_url}' + base_url + '\n\n')
+  app.run(host='0.0.0.0', port=port, debug=True)
